177d1079bd3e87bd47343be4af88a76b447616f8
kipperbot@gmail.com <kirzhner912>
2018-06-05 23:30:03 +0300

    2

---
+++ DZ02/task_01.py
@@ -0,0 +1,9 @@
+﻿# task 01
+a = int(input('Введите первое число\n>'))
+b = int(input('Введите второе число\n>'))
+if a > b:
+    print('>БОЛЬШЕ>')
+elif a < b:
+    print('<МЕНЬШЕ<')
+else:
+    print('=РАВНЫ=')
