39b5dfe493da50c8410edc0e7d3f181569cf0652
kipperbot@gmail.com <kirzhner912>
2018-06-10 16:19:51 +0300

    @lst

---
+++ DZ02/task_05.py
@@ -0,0 +1,26 @@
+﻿# a = str(input("Pls enter student's 1stname\n>"))
+# b = str(input("Pls enter student's 2stname\n>"))
+# c = str(input("Pls enter student's patronymic\n>"))
+# d = str(input("Pls enter student's group No.\n>"))
+a = 'Bw'
+b = 'sdwer'
+c = 'sw'
+d = 'DN3221'
+string = ['Лабораторная работа N 1', 'Выполнил(а): ст. гр.' + d, "{} {} {}".format(a, b, c)]
+
+def find_max(string):
+    strs = string[0]
+    for strs_max in string:
+        if int(len(strs)) < int(len(strs_max)):
+            strs = strs_max
+    return strs
+find_max(string)
+
+w = int(len(find_max(string))) + 2
+
+print ('*' * (w + 2))
+cstr = "*{:^%s}*" % w
+print(cstr.format(string[0]))
+print(cstr.format(string[1]))
+print(cstr.format(string[2]))
+print ('*' * (w + 2))
