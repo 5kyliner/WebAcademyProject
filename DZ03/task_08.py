d687764b9dc946bc0b5035bf8ab18ae9d1006023
$ky <kipperbot@gmail.com>
2018-06-14 00:11:00 +0300

    1

--- DZ03/task_08.py
+++ DZ03/task_08.py
@@ -1,18 +1,22 @@
-﻿#task08
+#task08
 """
 6*. Решить рекурсивно задачу нахождения n-го числа фиббоначи (см. https://ru.wikipedia.org/wiki/Числа_Фибоначчи)
 Пример
 >>fib(4)
 >>3
 """
-# n = input('Введите номер числа Фибоначчи\n>')
-n = 6
-
+n = int(input('Введите номер числа Фибоначчи\n>'))
 def fib(n):
-    for i in range(n):
-        fibn = [1, 1]
-        z = fibn[0] + fibn[1]
-        fibn.append(z)
-        return fibn[n]
-fib(n)
-print('Число Фиббоначи с номером ', n, '=', fib(n))
+    if n < 3:
+        return 2
+    return fib(n - 1) + fib(n -2)
+
+print('Число Фиббоначи с номером ', n, ':', fib(n))
+
+# n = int(input('Введите номер числа Фибоначчи\n>'))
+# def fib(n):
+#     a, b = 1, 1
+#     for i in range(n):
+#         a, b = b, a + b
+#     return b
+# print('Число Фиббоначи с номером ', n, ':', fib(n))
