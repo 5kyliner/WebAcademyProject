00177ac48c0afa34685bb7ccf86f4c81a49a310e
$ky <kipperbot@gmail.com>
2018-06-17 17:15:19 +0300

    55

--- DZ03/task_08.py
+++ DZ03/task_08.py
@@ -5,18 +5,18 @@
 >>fib(4)
 >>3
 """
-n = int(input('Введите номер числа Фибоначчи\n>'))
-def fib(n):
-    if n < 3:
-        return 2
-    return fib(n - 1) + fib(n -2)
-
-print('Число Фиббоначи с номером ', n, ':', fib(n))
-
 # n = int(input('Введите номер числа Фибоначчи\n>'))
 # def fib(n):
-#     a, b = 1, 1
-#     for i in range(n):
-#         a, b = b, a + b
-#     return b
+#     if n < 3:
+#         return 2
+#     return fib(n - 1) + fib(n -2)
+#
 # print('Число Фиббоначи с номером ', n, ':', fib(n))
+
+n = int(input('Введите номер числа Фибоначчи\n>'))
+def fib(n):
+    a, b = 1, 1
+    for i in range(n):
+        a, b = b, a + b
+    return b
+print('Число Фиббоначи с номером ', n, ':', fib(n))
