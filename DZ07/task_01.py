0de1979b08230c4cc09ade16d33034676dd88007
$ky <kipperbot@gmail.com>
2018-06-25 23:57:57 +0300

    `

---
+++ DZ07/task_01.py
@@ -0,0 +1,23 @@
+# class Car:
+#
+#     def __init__(self, model, color, year, price):
+#         self.model = model
+#         self.color = color
+#         self.year = year
+#         self.price = price
+#
+#     def __str__(self):
+#         string = f'model: {self.model}\ncolor: {self.color}' \
+#                  f'\nyear: {self.year}\nprice: {self.price}'
+#         return string
+#
+#     """
+#     model: Audi
+#     color: Red
+#     year: 1999
+#     price: $12000
+#     """
+#
+# car1 = Car('Audi', 'Red', '1999', '$12000')
+# print(car1)
+
